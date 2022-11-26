b679790d1829da7aee3b2fb2f72e338130f28045
Ziad Abo Alkamar <40515328+ziadaboalkamar@users.noreply.github.com>
2022-11-26 13:00:54 +0200

    update

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Store extends Model
 {
+    use SoftDeletes;
     use HasFactory;
     protected $fillable = ["logo","address","name"];
     public $timestamps = true;
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
+    use SoftDeletes;
     use HasFactory;
 protected $fillable = ["purchase_price","product_id","store_id"];
 public $timestamps = true;
